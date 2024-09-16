3583f140194e520fc73eb0505265318dcf7f1332
tyskill <tyskill@foxmail.com>
2024-09-17 02:02:17 +0800

    fix statusContainer NPE

--- src/drawio-client/app/Plugin.ts
+++ src/drawio-client/app/Plugin.ts
@@ -125,7 +125,8 @@ export default class Plugin {
 
     // Remove the status elements because this plugin manages saving the diagram
     app.statusContainer.remove();
-    app.statusContainer = null;
+    // `null` will cause NPE
+    // app.statusContainer = null;
     if (
       app.menubarContainer.parentElement.firstChild === app.menubarContainer &&
       app.menubarContainer.parentElement.childElementCount === 1
